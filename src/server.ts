a8df6061e39d1cd05880c3d28d3f642c486ccb19
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-21 02:34:08 +0600

    feat: add server dot env file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
+import "dotenv/config";
 import app from "./app";
+import { prisma } from "./lib/prisma";
 
 const PORT = process.env.PORT || 5000;
 
 async function main() {
   try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully");
     app.listen(PORT, () => {
       console.log(`Server Is Running On ${PORT}`);
     });
   } catch (error) {
     console.error("Error Starting the Server", error);
+    await prisma.$disconnect();
     process.exit(1);
   }
 }

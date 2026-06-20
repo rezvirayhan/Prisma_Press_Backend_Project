a2671ba8f32a880e03c6fb9cbe815f7bc84be4d1
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-21 02:42:58 +0600

    feat: add config file on index

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
 import "dotenv/config";
 import app from "./app";
 import { prisma } from "./lib/prisma";
+import config from "./config";
 
-const PORT = process.env.PORT || 5000;
+const PORT = config.port
 
 async function main() {
   try {
-    await prisma.$connect();
+    // await prisma.$connect();
     console.log("Connected to the database successfully");
     app.listen(PORT, () => {
       console.log(`Server Is Running On ${PORT}`);
     });
   } catch (error) {
     console.error("Error Starting the Server", error);
-    await prisma.$disconnect();
+    // await prisma.$disconnect();
     process.exit(1);
   }
 }

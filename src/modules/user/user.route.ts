b5eb2397c36c4b56ef6f000edb3523315cf7723d
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-22 15:25:04 +0600

    fix: user routes

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -6,55 +6,6 @@ import { prisma } from "../../lib/prisma";
 
 const router = Router();
 
-router.post("/register", async (req: Request, res: Response) => {
-  const { name, email, password, profilePhoto } = req.body;
-  const isUserExist = await prisma.user.findUnique({
-    where: { email },
-  });
-  if (isUserExist) {
-    throw new Error("User whit this email already exists");
-  }
-
-  const hashedPassword = await bcrypt.hash(
-    password,
-    Number(config.bcrypt_salt_rounds),
-  );
-  const createdUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  await prisma.profile.create({
-    data: {
-      userId: createdUser.id,
-      profilePhoto,
-    },
-  });
-
-  const user = await prisma.user.findUnique({
-    where: {
-      id: createdUser.id,
-      email: createdUser.email || email,
-    },
-    omit: {
-      password: true,
-    },
-    include: {
-      profile: true,
-    },
-  });
-
-  res.status(httpStatus.CREATED).json({
-    success: true,
-    statusCode: httpStatus.CREATED,
-    message: "User Registered Sucessfully",
-    data: {
-      user,
-    },
-  });
-});
+router.post("/register", );
 
 export const userRoutes = router;

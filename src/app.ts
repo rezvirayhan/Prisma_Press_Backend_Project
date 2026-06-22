f5113923d0b8e8268d2e9ca9328d2e9798d0e9e7
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-22 15:19:47 +0600

    fix: app ts file

--- src/app.ts
+++ src/app.ts
@@ -21,55 +21,6 @@ app.use(cookieParser());
 
 app.get("/", async (req: Request, res: Response) => {});
 
-app.post("/api/users/register", async (req: Request, res: Response) => {
-  const { name, email, password, profilePhoto } = req.body;
-  const isUserExist = await prisma.user.findUnique({
-    where: { email },
-  });
-  if (isUserExist) {
-    throw new Error("User whit this email already exists");
-  }
-
-  const hashedPassword = await bcrypt.hash(
-    password,
-    Number(config.bcrypt_salt_rounds),
-  );
-  const createdUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  await prisma.profile.create({
-    data: {
-      userId: createdUser.id,
-      profilePhoto,
-    },
-  });
-
-  const user = await prisma.user.findUnique({
-    where: {
-      id: createdUser.id,
-      email: createdUser.email || email,
-    },
-    omit: {
-      password: true,
-    },
-    include: {
-      profile: true,
-    },
-  });
-
-  res.status(httpStatus.CREATED).json({
-    success: true,
-    statusCode: httpStatus.CREATED,
-    message: "User Registered Sucessfully",
-    data: {
-      user,
-    },
-  });
-});
+app.post();
 
 export default app;

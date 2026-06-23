662decfc2780b55e862e8d26408e1a0f5b07f67f
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-24 02:16:41 +0600

    feat: add send response

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 import { userServices } from "./user.service";
-
 
 // const registerUser = async (req: Request, res: Response) => {
 //   try {

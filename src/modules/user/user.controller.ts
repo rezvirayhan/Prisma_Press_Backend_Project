4d95068f02b017c9af966931ab6075be4f3b1679
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-23 16:25:06 +0600

    fix: user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,17 +2,27 @@ import { Request, Response } from "express";
 import httpStatus from "http-status";
 import { userServices } from "./user.service";
 const registerUser = async (req: Request, res: Response) => {
-  const payload = req.body;
-  const user = await userServices.registerUserIntoDB(payload);
+  try {
+    const payload = req.body;
+    const user = await userServices.registerUserIntoDB(payload);
 
-  res.status(httpStatus.CREATED).json({
-    success: true,
-    statusCode: httpStatus.CREATED,
-    message: "User Registered Sucessfully",
-    data: {
-      user,
-    },
-  });
+    res.status(httpStatus.CREATED).json({
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User Registered Sucessfully",
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+      message: "Failked to register user ",
+      error: (error as Error).message,
+    });
+  }
 };
 
 export const userController = {

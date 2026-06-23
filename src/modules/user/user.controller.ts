ed1bc7602ed21bb1fb6d43b8754b63873f018a46
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-24 01:55:41 +0600

    remove user register

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -18,29 +18,29 @@ const catchAsync = (fn: RequestHandler) => {
   };
 };
 
-const registerUser = async (req: Request, res: Response) => {
-  try {
-    const payload = req.body;
-    const user = await userServices.registerUserIntoDB(payload);
+// const registerUser = async (req: Request, res: Response) => {
+//   try {
+//     const payload = req.body;
+//     const user = await userServices.registerUserIntoDB(payload);
 
-    res.status(httpStatus.CREATED).json({
-      success: true,
-      statusCode: httpStatus.CREATED,
-      message: "User Registered Sucessfully",
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-      message: "Failed to register user ",
-      error: (error as Error).message,
-    });
-  }
-};
+//     res.status(httpStatus.CREATED).json({
+//       success: true,
+//       statusCode: httpStatus.CREATED,
+//       message: "User Registered Sucessfully",
+//       data: {
+//         user,
+//       },
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//       success: false,
+//       statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+//       message: "Failed to register user ",
+//       error: (error as Error).message,
+//     });
+//   }
+// };
 
 export const userController = {
   registerUser,

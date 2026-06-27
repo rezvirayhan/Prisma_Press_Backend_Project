2e9eb170ef9771adcbea94ad103ac7fac5591db2
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-27 12:39:15 +0600

    feat: add auter routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
+import { authRoutes } from "./modules/auth/auth.routes";
 import { userRoutes } from "./modules/user/user.routes";
 
 const app: Application = express();
@@ -20,5 +21,6 @@ app.use(cookieParser());
 app.get("/", async (req: Request, res: Response) => {});
 
 app.use("/api/users", userRoutes);
+app.use("/api/users", authRoutes);
 
 export default app;

2cfdab64ccbf0107c3d1f35af3ffd1a6a5206961
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-27 12:51:34 +0600

    feat: login api is ready

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,20 @@
-const loginUser = (payload: {email: string, password: string}) =>{
+import bcrypt from "bcryptjs";
+import { prisma } from "../../lib/prisma";
+import { ILoginUser } from "./auth.interface";
 
-}
+const loginUser = async (payload: ILoginUser) => {
+  const { email, password } = payload;
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { email },
+  });
+  const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatched) {
+    throw new Error("Password Is INCORRECT");
+  }
+  return user;
+};
 
 export const authService = {
-  loginUser
-}
+  loginUser,
+};

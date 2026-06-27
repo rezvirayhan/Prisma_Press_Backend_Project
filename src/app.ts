a2fdf81433217b7808d2111a13134912991b7791
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-27 12:31:40 +0600

    fix: routes file name change

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
-import { userRoutes } from "./modules/user/user.route";
+import { userRoutes } from "./modules/user/user.routes";
 
 const app: Application = express();
 

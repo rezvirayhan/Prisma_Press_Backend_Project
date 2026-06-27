c66f83866811ebb58e69668711abf73f68fdb9c1
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-27 12:44:08 +0600

    feat: add auth controlers

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,22 @@
 import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { authService } from "./auth.service";
 
 const lginUser = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body;
+
+    const loginResult = await authService.loginUser(payload);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: " User logged in successfully",
+      data: loginResult,
+    });
+  },
 );
 
 export const authController = {

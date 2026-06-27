7bdd5e1b36b0749f180b17a467ad12bb464607eb
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-27 13:08:22 +0600

    feat: add accrss token and refresh token

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface";
-
 const loginUser = async (payload: ILoginUser) => {
   const { email, password } = payload;
   const user = await prisma.user.findUniqueOrThrow({
@@ -12,7 +12,36 @@ const loginUser = async (payload: ILoginUser) => {
   if (!isPasswordMatched) {
     throw new Error("Password Is INCORRECT");
   }
-  return user;
+
+  const accessToken = jwt.sign(
+    {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+    "accesssecret",
+    {
+      expiresIn: "1d",
+    },
+  );
+  const refreshToken = jwt.sign(
+    {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+    "refreshsecret",
+    {
+      expiresIn: "7d",
+    },
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+  };
 };
 
 export const authService = {

182c7f883fb9a3bc1b7c6aa5e4ff0dfaa195d67a
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-05 02:04:25 +0600

    fix: auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+import { JwtPayload } from "jsonwebtoken";
 import { Role } from "../../generated/prisma/enums";
-
+import config from "../config";
+import { prisma } from "../lib/prisma";
+import { catchAsync } from "../utils/catchAsync";
+import { jwtUtils } from "../utils/jwt";
 declare global {
   namespace Express {
     interface Request {
@@ -12,3 +17,50 @@ declare global {
     }
   }
 }
+
+const auth = (...requiredRoles: Role[]) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.accessToken
+      ? req.cookies.accessToken
+      : req.headers.authorization?.startsWith("Bearer")
+        ? req.headers.authorization?.split(" ")[1]
+        : req.headers.authorization;
+    if (!token) {
+      throw new Error(
+        "You Are not logged in. please log in to access this resource",
+      );
+    }
+    const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
+
+    if (!verifiedToken.success) {
+      throw new Error(verifiedToken.error);
+    }
+    const { email, name, id, role } = verifiedToken.data as JwtPayload;
+    if (requiredRoles.length && !requiredRoles.includes(role)) {
+      throw new Error(
+        "Forbidden You don't have permission to access this resource",
+      );
+    }
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+        email,
+        name,
+        role,
+      },
+    });
+    if (!user) {
+      throw new Error("User not found. please log in again");
+    }
+    if (user.activeStatus === "BLOCKED") {
+      throw new Error("Your Account has been block. please contact support");
+    }
+    req.user = {
+      email,
+      name,
+      id,
+      role,
+    };
+    next();
+  });
+};

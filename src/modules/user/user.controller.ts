b9d6632d44a500076c39cef2ae9d69e96b802923
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-05 02:23:22 +0600

    feat: update my profile

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -70,7 +70,20 @@ const getMyProfile = catchAsync(
 );
 
 const updateMyProfile = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user?.id as string;
+    const payload = req.body;
+    const updateProfile = await userServices.updateMyProfileInDB(
+      userId,
+      payload,
+    );
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User Profile  updated successfully",
+      data: { updateProfile },
+    });
+  },
 );
 
 export const userController = {

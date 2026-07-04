fe65fa8a5134c771ec16e1e8633a113863777ad5
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-05 02:05:33 +0600

    fix: auth middleware

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,62 +1,11 @@
-import { NextFunction, Request, Response, Router } from "express";
-import { JwtPayload } from "jsonwebtoken";
+import { Router } from "express";
 import { Role } from "../../../generated/prisma/enums";
-import config from "../../config";
-import { prisma } from "../../lib/prisma";
-import { catchAsync } from "../../utils/catchAsync";
-import { jwtUtils } from "./../../utils/jwt";
+import { auth } from "../../middlewares/auth";
 import { userController } from "./user.controller";
 
 const router = Router();
 
 router.post("/register", userController.registerUser);
-
-const auth = (...requiredRoles: Role[]) => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies.accessToken
-      ? req.cookies.accessToken
-      : req.headers.authorization?.startsWith("Bearer")
-        ? req.headers.authorization?.split(" ")[1]
-        : req.headers.authorization;
-    if (!token) {
-      throw new Error(
-        "You Are not logged in. please log in to access this resource",
-      );
-    }
-    const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
-
-    if (!verifiedToken.success) {
-      throw new Error(verifiedToken.error);
-    }
-    const { email, name, id, role } = verifiedToken.data as JwtPayload;
-    if (requiredRoles.length && !requiredRoles.includes(role)) {
-      throw new Error(
-        "Forbidden You don't have permission to access this resource",
-      );
-    }
-    const user = await prisma.user.findUnique({
-      where: {
-        id,
-        email,
-        name,
-        role,
-      },
-    });
-    if (!user) {
-      throw new Error("User not found. please log in again");
-    }
-    if (user.activeStatus === "BLOCKED") {
-      throw new Error("Your Account has been block. please contact support");
-    }
-    req.user = {
-      email,
-      name,
-      id,
-      role,
-    };
-    next();
-  });
-};
 
 router.get(
   "/me",

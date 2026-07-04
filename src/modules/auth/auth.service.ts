a026f15c369c89ae8e03735bc2bf03c808c04e30
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-05 01:59:55 +0600

    fix: auth services check user active or block

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -9,6 +9,9 @@ const loginUser = async (payload: ILoginUser) => {
   const user = await prisma.user.findUniqueOrThrow({
     where: { email },
   });
+  if (user.activeStatus === "BLOCKED") {
+    throw new Error("Your Account has been block. please contact support");
+  }
   const isPasswordMatched = await bcrypt.compare(password, user.password);
 
   if (!isPasswordMatched) {

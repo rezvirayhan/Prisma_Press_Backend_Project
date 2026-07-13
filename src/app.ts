8bf51b98e3ee75b53e321642de6ee64c1121c932
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-13 12:56:04 +0600

    feat: add post api

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import httpStatus from "http-status";
 import config from "./config";
 const app: Application = express();
 app.use(
@@ -15,6 +16,15 @@ app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
+});
+
+app.post("/api/users/register", async (req: Request, res: Response) => {
+  const payload = req.body;
+  console.log(payload);
+
+  res
+    .status(httpStatus.CREATED)
+    .json({ message: "User registered successfully" });
 });
 
 export default app;

eb8372bdd3fafc5fb23019908d42b642595bb189
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-21 00:24:01 +0600

    fix: router feat: controller

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -3,61 +3,10 @@ import { Request, Response, Router } from "express";
 import HttpStatus from "http-status";
 import config from "../../config";
 import { prisma } from "../../lib/prisma";
+import { userController } from "./user.controller";
 
 const router = Router();
 
-router.post("/register", async (req: Request, res: Response) => {
-  const { name, email, password, ProfilePhoto } = req.body;
-
-  const isUserExist = await prisma.user.findUnique({
-    where: { email },
-  });
-
-  if (isUserExist) {
-    throw new Error("User with this email already Exists");
-  }
-
-  const hashedPassword = await bcrypt.hash(
-    password,
-    Number(config.bcrypt_salt_rounds),
-  );
-
-  const createdUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  await prisma.profile.create({
-    data: {
-      userId: createdUser.id,
-      ProfilePhoto,
-    },
-  });
-
-  const user = await prisma.user.findUnique({
-    where: {
-      id: createdUser.id,
-      email: createdUser.email || email,
-    },
-    include: {
-      profile: true,
-    },
-    omit: {
-      password: true,
-    },
-  });
-
-  res.status(HttpStatus.CREATED).json({
-    success: true,
-    statusCode: HttpStatus.CREATED,
-    message: "User Register Succesfully",
-    data: {
-      user,
-    },
-  });
-});
+router.post("/register", userController.registerUser );
 
 export const userRoutes = router;

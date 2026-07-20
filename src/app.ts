90dac645348fd5357f17178f602490f596ad004e
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-20 23:55:30 +0600

    feat: add user register api

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import HttpStatus from "http-status";
 import config from "./config";
 const app: Application = express();
 
@@ -15,6 +16,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
+});
+
+app.post("/api/users/register", async (req: Request, res: Response) => {
+  const payload = req.body;
+  console.log(payload);
+  res.status(HttpStatus.CREATED).json({
+    message: "User Register Succesfully",
+  });
 });
 
 export default app;

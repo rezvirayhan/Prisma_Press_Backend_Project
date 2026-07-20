bf4fc7024e469459889aaf66f58634a5d1e19f57
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-21 01:01:43 +0600

    fix: error message

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -3,17 +3,27 @@ import HttpStatus from "http-status";
 import { userService } from "./user.service";
 
 const registerUser = async (req: Request, res: Response) => {
-  const payload = req.body;
+  try {
+    const payload = req.body;
 
-  const user = await userService.registerUserIntoDB(payload);
-  res.status(HttpStatus.CREATED).json({
-    success: true,
-    statusCode: HttpStatus.CREATED,
-    message: "User Register Successfully",
-    data: {
-      user,
-    },
-  });
+    const user = await userService.registerUserIntoDB(payload);
+    res.status(HttpStatus.CREATED).json({
+      success: true,
+      statusCode: HttpStatus.CREATED,
+      message: "User Register Successfully",
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: "Failed to register User",
+      error: (error as Error).message,
+    });
+  }
 };
 
 export const userController = {

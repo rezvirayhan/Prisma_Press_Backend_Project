d32671edfd4fe1e5f740c51ce7401796abd1facc
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-20 17:28:57 +0600

    feat: successfully server on running

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
 
 export default app;

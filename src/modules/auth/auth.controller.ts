73de3d7693fcae01ad6b24b396bcee47adecf640
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-22 00:16:35 +0600

    fix: login user successfully

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,21 @@
+import HttpStatus from "http-status";
+
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { authService } from "./auth.service";
 
 const loginUser = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body;
+    const loginResult = await authService.loginUser(payload);
+    sendResponse(res, {
+      success: true,
+      statusCode: HttpStatus.OK,
+      message: "User Login Succesfully",
+      data: loginResult,
+    });
+  },
 );
 
 export const authController = {
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,20 @@
-const loginUser = () => {};
+import bcrypt from "bcryptjs";
+import { prisma } from "../../lib/prisma";
+import { ILoginUser } from "./auth.interface";
+
+const loginUser = async (payload: ILoginUser) => {
+  const { email, password } = payload;
+
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { email },
+  });
+  const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatched) {
+    throw new Error("Password Is Incorrect");
+  }
+  return user;
+};
 
 export const authService = {
   loginUser,

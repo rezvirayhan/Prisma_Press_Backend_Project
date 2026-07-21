796db2abd32f0c58564267eaa4cbd035119792bc
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-21 23:59:29 +0600

    feat: add send response

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,31 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import HttpStatus from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 import { userService } from "./user.service";
-
-type TMeta = {
-  page: number;
-  limit: number;
-  total: number;
-};
-
-type TResponseData<T> = {
-  success: boolean;
-  statusCode: number;
-  message: string;
-  data: T;
-  meta?: TMeta;
-};
-
-const sendResponse = <T>(res: Response, data: TResponseData<T>) => {
-  res.status(data.statusCode).json({
-    success: data.success,
-    statusCode: data.statusCode,
-    message: data.message,
-    data: data.data,
-    meta: data.meta,
-  });
-};
 
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {

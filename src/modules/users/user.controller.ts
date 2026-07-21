f5c1c76e82e71ab5e672e240091e29ab7ac4ff7d
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-21 23:56:01 +0600

    fix: send response

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -17,7 +17,15 @@ type TResponseData<T> = {
   meta?: TMeta;
 };
 
-const sendResponse = <T>(res: Response, data: TResponseData<T>) => {};
+const sendResponse = <T>(res: Response, data: TResponseData<T>) => {
+  res.status(data.statusCode).json({
+    success: data.success,
+    statusCode: data.statusCode,
+    message: data.message,
+    data: data.data,
+    meta: data.meta,
+  });
+};
 
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {

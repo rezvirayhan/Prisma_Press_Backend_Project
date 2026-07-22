d670bb0fc5caf482720ce0f686b430f6115e6f8b
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-22 17:01:54 +0600

    fix: auth middlware

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -10,6 +10,9 @@ const loginUser = async (payload: ILoginUser) => {
   const user = await prisma.user.findUniqueOrThrow({
     where: { email },
   });
+  if (user.activeStatus === "BLOCKED") {
+    throw new Error("Your account has been blocked. please contact support");
+  }
   const isPasswordMatched = await bcrypt.compare(password, user.password);
 
   if (!isPasswordMatched) {
--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response, Router } from "express";
-import httpStatus from "http-status";
+import { JwtPayload } from "jsonwebtoken";
 import { Role } from "../../../generated/prisma/enums";
 import config from "../../config";
+import { prisma } from "../../lib/prisma";
+import { catchAsync } from "../../utils/catchAsync";
 import { jwtUtils } from "../../utils/jwt";
 import { userController } from "./user.controller";
 const router = Router();
@@ -20,38 +22,92 @@ declare global {
 }
 
 router.post("/register", userController.registerUser);
-router.get(
-  "/me",
-  (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.cookies);
-    const { accessToken } = req.cookies;
-    const verifiedToken = jwtUtils.verifyToken(
-      accessToken,
-      config.jwt_access_secret,
-    );
-    if (typeof verifiedToken === "string") {
-      throw new Error(verifiedToken);
-    }
-    const { email, name, id, role } = verifiedToken;
-    const requiredRoles = [Role.ADMIN, Role.USER, Role.AUTHOR];
 
-    if (!requiredRoles.includes(role)) {
-      return res.status(403).json({
-        success: false,
-        statusCode: httpStatus.FORBIDDEN,
-        message: "Forbidden You don't have permission to access this resource",
-      });
+// Start Hire Order function on middleware
+const auth = (...requiredRoles: Role[]) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token =
+      req.cookies.accessToken || req.headers.authorization?.startsWith("Bearer")
+        ? req.headers.authorization?.split(" ")[1]
+        : req.headers.authorization;
+
+    if (!token) {
+      throw new Error(
+        "You are not logged in. Please log in to access this resource",
+      );
     }
 
+    const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
+
+    if (!verifiedToken.success) {
+      throw new Error(verifiedToken.error);
+    }
+    const { email, name, id, role } = verifiedToken.data as JwtPayload;
+    if (requiredRoles.length && !requiredRoles.includes(role)) {
+      throw new Error(
+        "Forbidden . You don't have permission to access this resource",
+      );
+    }
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+        email,
+        name,
+        role,
+      },
+    });
+    if (!user) {
+      throw new Error("User not found. Please log in again");
+    }
+    if (user.activeStatus === "BLOCKED") {
+      throw new Error("Your account has been blocked. please contact support");
+    }
     req.user = {
       email,
       name,
       id,
       role,
     };
-
     next();
-  },
+  });
+};
+// End Hire Order function on middleware
+
+router.get(
+  "/me",
+  // (req: Request, res: Response, next: NextFunction) => {
+  //   console.log(req.cookies);
+  //   const { accessToken } = req.cookies;
+  //   const verifiedToken = jwtUtils.verifyToken(
+  //     accessToken,
+  //     config.jwt_access_secret,
+  //   );
+
+  //   if (!verifiedToken.success) {
+  //     throw new Error(verifiedToken.error);
+  //   }
+
+  //   const { email, name, id, role } = verifiedToken.data as JwtPayload;
+  //   const requiredRoles = [Role.ADMIN, Role.USER, Role.AUTHOR];
+
+  //   if (!requiredRoles.includes(role)) {
+  //     return res.status(403).json({
+  //       success: false,
+  //       statusCode: httpStatus.FORBIDDEN,
+  //       message: "Forbidden You don't have permission to access this resource",
+  //     });
+  //   }
+
+  //   req.user = {
+  //     email,
+  //     name,
+  //     id,
+  //     role,
+  //   };
+
+  //   next();
+  // },
+  auth(Role.ADMIN, Role.USER, Role.AUTHOR),
   userController.getMyProfile,
 );
 

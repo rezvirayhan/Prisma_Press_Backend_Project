0cd0523fec8081e25b7d7d3154c0e59d03dd3f32
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-24 02:30:54 +0600

    feat: create post serives all pleate

---
+++ src/modules/post/post.service.ts
@@ -0,0 +1,17 @@
+const createPost = () => {};
+const getAllPost = () => {};
+const getPostById = () => {};
+const updatePost = () => {};
+const deletePost = () => {};
+const getPostStatus = () => {};
+const getMyPost = () => {};
+
+export const postService = {
+  createPost,
+  getAllPost,
+  getPostById,
+  updatePost,
+  deletePost,
+  getPostStatus,
+  getMyPost,
+};

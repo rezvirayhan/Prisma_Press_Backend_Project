c70fd77a2b8d562b5bf4efde17001088644f1575
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-07-24 21:53:26 +0600

    feat:  create post done

--- src/modules/post/post.interface.ts
+++ src/modules/post/post.interface.ts
@@ -3,8 +3,8 @@ import { PostStatus } from "../../../generated/prisma/enums";
 export interface ICreatePostPayload {
   title: string;
   content: string;
-  thumbnail ? : string;
-  isFeatured ?: boolean;
-  status ?: PostStatus
-  tags : string[]
+  thumbnail?: string;
+  isFeatured?: boolean;
+  status?: PostStatus;
+  tags: string[];
 }
